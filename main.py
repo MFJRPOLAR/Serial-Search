7b0140115c3662ac0305e60d5cec3af9c9d5433d
Sue Ceklosky <cekloskys@chc.edu>
2023-09-13 10:47:08 -0400

    First-Version-Serial-Search

---
+++ main.py
@@ -0,0 +1,27 @@
+from serialsearch import *
+
+print(__name__)
+def main():
+    # Set up local variables to store the list, first, size, and target
+    a = []
+    first = 0
+    size = 0 
+    target = 0 
+
+    # Prompt the user for the elements adn inout into the list
+    a = list(map(int,input("Enter seven numbers seperated by space: ").split()))
+
+    # Prompt the user for the index at which the search will begin 
+    first = int(input("Enter the index at whcih the search will begin: "))
+
+    # Prompt the user for the number of elements to search 
+    size = int(input("Enter the size of the list that will be searched: "))
+
+    # Prompt the user for the target
+    target = int(input("Enter the target value to search for: "))
+
+    # Call search and display its return
+    print("Target found at index....",search(a,first,size,target))
+    
+if __name__ == "__main__":
+    main()
